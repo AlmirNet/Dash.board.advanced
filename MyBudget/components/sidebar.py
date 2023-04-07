116ac9b3456e118e37467a33a4ead13725da036e
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-04-07 19:11:14 -0300

    Adição dos campos Dashboards e Extratos

--- MyBudget/components/sidebar.py
+++ MyBudget/components/sidebar.py
@@ -10,19 +10,36 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 
-
-
-
-
-
 # ========= Layout ========= #
 layout = dbc.Col([
-                
-            ])
+    html.H1("MyBestBudget", className='text-primary'),
+    html.P('By ALMIRNET', className='text-info'),
+    html.Hr(),
 
+    # Seção PERFIL --------------------------------
+    dbc.Button(
+        id='botao_avatar',
+        children=html.Img(src='/assets/img_hom.png', id='avatar_change',alt='Avatar', className='perfil_avatar')
+    ),
 
+    # Seção NOVO -------------------------------------
+    dbc.Row([
+        dbc.Col([
+            dbc.Button(color='success',
+                       id='open-novo-receita', children=['Receita'])
+        ], width=6),
 
+        dbc.Col([
+            dbc.Button(color='danger', id='open-novo-despesa', children=['Despesa'])
+        ], width=6),
+    ]),
 
+    # Seção NAV ------------------------------------
+    html.Hr(),
+    html.Nav([
+        dbc.NavLink('Dashboard', href='/dashboards', active='exact'),
+        dbc.NavLink('Extratos', href='/extratos', active='exact')
+    ])
 
-# =========  Callbacks  =========== #
-# Pop-up receita
+    
+])
--- MyBudget/myindex.py
+++ MyBudget/myindex.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
-from app import *
+from app import app
 from components import sidebar, dashboards, extratos
 
 
@@ -20,10 +20,10 @@ app.layout = dbc.Container(children=[
         dbc.Col([
             dcc.Location(id='url'),
             sidebar.layout
-        ], md=2, style={'background-color': 'red', 'height': '1080px' }),
+        ], md=2),
         dbc.Col([
             content
-        ], md=10, style={'background-color': 'blue', 'height': '1080px' }),
+        ], md=10),
     ])
 
 ], fluid=True,)
