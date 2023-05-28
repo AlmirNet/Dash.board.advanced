74588d934a0ce9f8932760435aa2015548244bb5
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-05-28 00:47:14 -0300

    Continuando montagem da dashboard.

--- MyBudget/components/dashboards.py
+++ MyBudget/components/dashboards.py
@@ -19,19 +19,88 @@ card_icon = {
 
 # =========  Layout  =========== #
 layout = dbc.Col([
-        dbc.Row([
-            dbc.Col([
-                dbc.CardGroup([
-                    html.Legend('Saldo'),
-                    html.H5('R$5400', id='p-saldo-dashboards', style={})
-                ], style={'padding-left': '20px', 'padding-top': '10px'}),
-                dbc.Card([
-                    html.Div(className='fa fa-university')
-                ])
-            ])
-        ])
-    ])
+    dbc.Row([
+        dbc.Col([
+            dbc.CardGroup([
+                html.Legend('Saldo'),
+                html.H5('R$5400', id='p-saldo-dashboards', style={})
+            ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            dbc.Card(
+                [
+                    html.Div(className='fa fa-university', style=card_icon),
+                ],
+                color='warning',
+                style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}
+            )
+        ], width=4),
+        dbc.Col([
+            dbc.CardGroup([
+                html.Legend('Receita'),
+                html.H5('R$5400', id='p-receita-dashboards', style={})
+            ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            dbc.Card(
+                [
+                    html.Div(className='fa fa-smile-o', style=card_icon),
+                ],
+                color='success',
+                style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}
+            )
+        ], width=4),
+        dbc.Col([
+            dbc.CardGroup([
+                html.Legend('Despesa'),
+                html.H5('R$5400', id='p-despesa-dashboards', style={})
+            ], style={'padding-left': '20px', 'padding-top': '10px'}),
+            dbc.Card(
+                [
+                    html.Div(className='fa fa-meh-o', style=card_icon),
+                ],
+                color='danger',
+                style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}
+            )
+        ], width=4)
+    ], style={'margin': '10px'}),
 
+    dbc.Row([
+    dbc.Col([
+        dbc.Card([
+            html.Legend("Filtrar lançamentos", className="card-title"),
+            html.Label("Categoria-receita"),
+            html.Div(
+                dcc.Dropdown(
+                    id="dropdown-receita",
+                    clearable=False,
+                    style={"width": "100%"},
+                    persistence=True,
+                    persistence_type="session",
+                    multi=True
+                )
+            ),
+
+            html.Label("Categoria das despesas", style={"margin-top": "10px"}),
+            dcc.Dropdown(
+                id="dropdown-despesas",
+                clearable=False,
+                style={"width": "100%"},
+                persistence=True,
+                persistence_type="session",
+                multi=True
+            )
+        ]),
+
+        html.Legend("Período de Análise", style={"margin-top": "10px"}),
+        dcc.DatePickerRange(
+            month_format='Do MMM, YY',
+            end_date_placeholder_text='Data...',
+            start_date=datetime(2022, 4, 1).date(),
+            end_date=datetime.today() + timedelta(days=31),
+            updatemode='singledate',
+            style={'z-index': '100'}
+        )
+    ], width=4)
+    
+])
+])
 
 
 # =========  Callbacks  =========== #
