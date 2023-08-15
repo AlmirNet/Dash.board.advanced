97dcda6ab4568a781856ce98bb6bdc6841c8471e
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-08-15 00:02:33 -0300

    Adicionando tabela, grafico e card.

--- MyBudget/globals.py
+++ MyBudget/globals.py
@@ -1,1 +1,5 @@
 # Em components/dashboard existe esta importação 'from globals import *' importando esta pasta globals
+import pandas as pd
+import os 
+
+if ("df_despesas.csv" in os.listdir()) and ("df_receitas.csv" in os.listdir()):
